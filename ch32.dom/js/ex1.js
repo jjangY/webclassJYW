bfa414d9df6ae9cb9f6f7ed065e856d3736ff8af
장예원 <aodtnsdode@naver.com>
2026-09-16 15:48:47 +0900

    0916

--- ch32.dom/js/ex1.js
+++ ch32.dom/js/ex1.js
@@ -1,21 +1,21 @@
 document.addEventListener('DOMContentLoaded',()=>{
     
-    let foniSizeBase = 20;
+    let fontSizeBase = 20;
     //크게 버튼 눌렀을 때 동작하는 기능
     const html = document.querySelector("html");
     const btnFontUp = document.querySelector('#btn-font-up');
     btnFontUp.addEventListener('click',()=>{
-        if(foniSizeBase>40){return}
-        foniSizeBase++;
-        html.style.fontSize = foniSizeBase +'px';
+        if(fontSizeBase>40){return}
+        fontSizeBase++;
+        html.style.fontSize = fontSizeBase +'px';
     });
 
     //작게 버튼 눌렀을 때 동작하는 기능(글씨크기 12이하는 작동x)
     const btnFontDown = document.querySelector('#btn-font-down');
     btnFontDown.addEventListener('click',()=>{
-        if(foniSizeBase<=12){return}
-        foniSizeBase--;
-        html.style.fontSize = foniSizeBase +'px';
+        if(fontSizeBase<=12){return}
+        fontSizeBase--;
+        html.style.fontSize = fontSizeBase +'px';
     });
 
     const btnToggle = document.querySelector("#btn-toggle");
@@ -30,13 +30,11 @@ document.addEventListener('DOMContentLoaded',()=>{
     btnToggle.addEventListener('click', function(){
         if(btnState){
             //이미지를 닫기 이미지로 교체
-            this.children[0].setAttribute('src','./img/close.svg');
-            this.children[0],setAttribute('alt','닫기');
+            this.children[0].setAttribute('src','./img/close.svg').setAttribute('alt','닫기');
             btnState = true;
         }else{
             //이미지를 메뉴 이미지로 교체
-            this.children[0].setAttribute('src','./img/menu.svg');
-           this.children[0],setAttribute('alt','메뉴');
+            this.children[0].setAttribute('src','./img/menu.svg').setAttribute('alt','메뉴');
             btnState = false;
         }
 
